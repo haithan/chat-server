31aac28c56bb2091c21fafa1753c3bb639b67403
vinh <vinhtt0305@gmail.com>
2015-11-19 15:08:38 +0700

    message queue

--- config/config.js
+++ config/config.js
@@ -14,7 +14,11 @@ var config = {
     main_web: 'lvh.me:3000',
     ggSenderId: 'AIzaSyBmFcDkIMWiNTU8cClehEAR8s5Fr1cSlV4',
     env: 'development',
-    rails_ip: '127.0.0.1'
+    rails_ip: '127.0.0.1',
+    notify_queue: 'notification',
+    mq_token: 'hgmZjraRGALuOKVAzkSm',
+    mq_id: '56498da54aa03100090000b1',
+    mq_host: 'mq-aws-eu-west-1-1.iron.io'
   },
 
   test: {
@@ -41,7 +45,11 @@ var config = {
     main_web: 'https://ymeet.me',
     ggSenderId: 'AIzaSyBmFcDkIMWiNTU8cClehEAR8s5Fr1cSlV4',
     env: 'production',
-    rails_ip: '103.56.157.70'
+    rails_ip: '103.56.157.70',
+    notify_queue: 'msg-notifi',
+    mq_token: 'hgmZjraRGALuOKVAzkSm',
+    mq_id: '564d47104aa0310009000139',
+    mq_host: 'mq-aws-eu-west-1-1.iron.io'
   }
 
 };
