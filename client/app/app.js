fc810248ed1a67b3e8850eb54cb6705960095430
haithan <tuyen055@gmail.com>
2015-11-26 14:18:11 +0700

    set config for test environment" ;

--- client/app/app.js
+++ client/app/app.js
@@ -28,7 +28,7 @@ angular.module('chatApp', [
 
   .factory('socket', function (socketFactory) {
     // var ioServer = 'http://chat.lvh.me:8123';
-    var ioServer = 'https://chat.ymeet.me';
+    var ioServer = 'https://chat2.ymeet.me';
     var myIoSocket = io.connect(ioServer, {
       'reconnection': true,
       // 'reconnectionAttempts': true
